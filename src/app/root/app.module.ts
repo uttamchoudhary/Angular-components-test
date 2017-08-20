84e4ee892c3c550537ca1cb4ddb929e93d0b588c
uprata <upratap@sapient.com>
2017-08-21 00:38:59 +0530

    dropdown module added

--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DropdownModule } from '../dropdown/dropdown.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
