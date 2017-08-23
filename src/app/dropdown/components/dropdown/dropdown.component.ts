6c738d834b9f746363755c56fb833486666f9f83
uprata <upratap@sapient.com>
2017-08-24 01:05:09 +0530

    documenr click close functionality

--- src/app/dropdown/components/dropdown/dropdown.component.ts
+++ src/app/dropdown/components/dropdown/dropdown.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'ng-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.scss']
+  styleUrls: ['./dropdown.component.scss'],
+  host: {
+    '(document:click)': 'closeDropdown($event)',
+  }
 })
 export class DropdownComponent implements OnInit {
   @Input() options: Array<any>;
   @Input() selected: number;
   @Output() change = new EventEmitter();
+  
+  selectedItem: any;
+  active:boolean = false;
+  constructor(private _eref: ElementRef) { }
 
-  constructor() { }
+
+  closeDropdown(event){
+    if (!this._eref.nativeElement.contains(event.target))
+    this.active = false;
+  }
 
   ngOnInit() {
+    this.selectedItem = this.options[this.selected - 1];
   }
   
   changeValue(index){
+    this.selectedItem = this.options[index];
     this.change.emit(index+1);
   }
 }
