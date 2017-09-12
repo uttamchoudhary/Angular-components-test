2a6fe0dc8f97629faa6cbe87150df319ffac820c
uprata <upratap@sapient.com>
2017-09-13 01:56:29 +0530

    Carousel compnent added

--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -7,18 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  {
-  options = ['Angular','react','Vue','Ember'];
-  selected = 2;
-  settings: Settings;
-  displayed;
-  constructor(){
-    this.settings =  {
-      output:'value',
-      isDatalist: true,      
-      caretClass: 'icon-dropdown'
-    }
-  }
-  changeValue(index){
-    this.displayed = index;
-  }
+  
 }
--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -1,3 +1,4 @@
+import { CarouselModule } from './../carousel/carousel.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    DropdownModule
+    DropdownModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
