07e8cd6e876077fc649060fb4f6569c6c8eea3b4
uprata <upratap@sapient.com>
2017-09-15 02:11:57 +0530

    circular slider functionality added

--- src/app/root/app.component.ts
+++ src/app/root/app.component.ts
@@ -1,4 +1,4 @@
-import { Settings } from 'ng-custom-select';
+import { Image } from './../carousel/model/image.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  {
+  images: Image[] = [
+    { "caption": "We are covered", "media": "../../assets/images/dashboard.PNG" },
+    { "caption": "Generation Gap", "media": "../../assets/images/dashborad1.PNG" },
+    { "caption": "Potter Me", "media": "../../assets/images/Expense.PNG" },
+    { "caption": "Pre-School Kids", "media": "../../assets/images/expense3.PNG" },
+    { "caption": "Young Peter Cech", "media": "../../assets/images/expense4.PNG" } 
+  ];
   
 }
