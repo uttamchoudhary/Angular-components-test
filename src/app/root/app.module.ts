38b1ad92908a1581cdc55e18bcfab27abe5bd33c
uttamchoudhary <uttampratapchoudhary@gmail.com>
2017-11-23 15:01:43 +0530

    Created base setup for component testing

--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { DropdownModule } from 'ng-custom-select';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    DropdownModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
